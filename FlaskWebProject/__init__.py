d87ce5b185e7176d9efe8eb44d56ec8520b34ade
Himanshu Chouhan <hchouhan3654@gmail.com>
2021-03-21 15:03:42 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,7 +10,23 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 # TODO: Add any logging levels and handlers with app.logger
+app.logger.setLevel(logging.WARNING)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
+
+if log:
+     if log == 'info':
+         app.logger.info('No issue.')
+     elif log == 'warning':
+         app.logger.warning('Warning occurred.')
+     elif log == 'error':
+         app.logger.error('Error occurred.')
+     elif log == 'critical':
+         app.logger.critical('Critical error occurred.')
+  
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
